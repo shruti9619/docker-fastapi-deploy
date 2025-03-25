6477c9d1ae4a44d4b53d7be44f491c8040c2b64e
shruti <shruti.personified@gmail.com>
2025-03-26 00:28:47 +0530

    feat: init poe

--- src/docker_fastapi_deploy/train.py
+++ src/docker_fastapi_deploy/train.py
@@ -1,15 +1,28 @@
+import argparse
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-# Load data
-iris = load_iris()
-X, y = iris.data, iris.target
 
-# Train model
-model = RandomForestClassifier(n_estimators=100, verbose=1)
-model.fit(X, y)
+def train_model(model_name: str):
+    # Load data
+    iris = load_iris()
+    X, y = iris.data, iris.target
 
-# Save model
-joblib.dump(model, "model.pkl")
-print("Model trained and saved as model.pkl")
+    # Train model
+    model = RandomForestClassifier(n_estimators=100, verbose=1)
+    model.fit(X, y)
+
+    # Save model
+    joblib.dump(model, model_name)
+    print(f"Model trained and saved as {model_name}")
+
+
+def init_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-name", type=str, default="model.pkl")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = init_args()
+    train_model(args.model_name)
